c5230d20c3c3c43f801893ad865ba1dc5271f420
Christopher Campbell <clccodes@yahoo.com>
2025-03-10 17:52:36 -0400

    Initial commit

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+import { words } from "./words"
+
+export function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length)
+    return words[randomIndex]
+}
+
+export function getFarewellText(language) {
+    const options = [
+        `Farewell, ${language}🫡`,
+        `Adios, ${language}👋`,
+        `R.I.P., ${language}☠️`,
+        `We'll miss you, ${language}!`,
+        `Oh no, not ${language}!`,
+        `${language} bites the dust!`,
+        `Gone but not forgotten, ${language}🎗️`,
+        `The end of ${language} as we know it🔚`,
+        `Off into the sunset, ${language}🌅`,
+        `${language}, it's been real!`,
+        `${language}, your watch has ended👀`,
+        `${language} has left the building!`
+    ];
+
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
